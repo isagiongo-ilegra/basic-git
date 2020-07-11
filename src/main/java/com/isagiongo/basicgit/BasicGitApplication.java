7112efd010b831024c16dda79068d24f3fd8bc23
Isadora Giongo <isagiongo@gmail.com>
2020-07-11 20:31:48 -0300

    Sending my first project to git

---
+++ src/main/java/com/isagiongo/basicgit/BasicGitApplication.java
@@ -0,0 +1,13 @@
+package com.isagiongo.basicgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isagiongo/basicgit/BasicGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isagiongo.basicgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
